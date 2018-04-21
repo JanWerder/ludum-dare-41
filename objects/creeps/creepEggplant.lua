49fd3415e5041e0f252955d3a8ee9f65bb0e7b24
Jan <jackflash5500@gmail.com>
2018-04-21 15:35:59 +0200

    More creeps

---
+++ objects/creeps/creepEggplant.lua
@@ -0,0 +1,21 @@
+require "objects/Creeps/Creep"
+
+CreepEggplant = Class{
+	__includes = Creep;
+	
+	init = function(self, x, y)
+		Creep.init(self, x, y)
+		
+		--- Creep Customizing --- 
+		-- damage Var's
+		self:setLife(10)
+		self:setSpeed(40)
+		self:setWidth(32)
+		self:setHeight(32)
+		
+		-- general stuff
+		self:setName('Eggplant')
+		self:setImage(love.graphics.newImage("img/eggplant.png"))
+	end
+}	
+
