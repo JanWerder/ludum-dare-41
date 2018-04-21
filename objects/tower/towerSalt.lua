fb5819f2241f68f7ed3eb1eed4777cabdc2a2ef2
Jan <jackflash5500@gmail.com>
2018-04-21 20:41:12 +0200

    All towers

---
+++ objects/tower/towerSalt.lua
@@ -0,0 +1,29 @@
+TowerSalt = Class{
+	__includes = Tower;
+	
+	init = function(self, x, y)
+		Tower.init(self, x, y)
+		
+		--- Tower Customizing --- 
+		-- damage Var's
+		self:setRange(325)
+		self:setDamage(2)
+		self:setShootCount(15)
+		
+		-- general stuff
+		self:setName('Catapult')
+		self:setImage(love.graphics.newImage("img/salt_still.png"))
+		self:setImageShootLength(2)
+		self:setImageShoot(love.graphics.newImage("img/salt.png"))
+		
+		self.oliveDrop = love.graphics.newImage("img/oliveoil_drop.png")
+		self.dropSpeed = 300
+	end
+}	
+
+function TowerSalt:shoot(creeps)
+	if creeps[1] then		
+		table.insert(self.projectiles, Projectile( self.worldX, self.worldY, self.oliveDrop, self.dropSpeed, creeps[1], self.damage, self.name))
+	end
+end
+
