a479250b10e774c14f568bce8e0bb45b6b258e20
Jan <jackflash5500@gmail.com>
2018-04-21 22:28:13 +0200

    tileset change

---
+++ maps/kitchen_tileset.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="kitchen" tilewidth="32" tileheight="32" tilecount="32" columns="8">
+ <image source="../img/kitchen_tilesheet.png" width="256" height="128"/>
+ <tile id="11">
+  <properties>
+   <property name="path" value="true"/>
+  </properties>
+ </tile>
+</tileset>
