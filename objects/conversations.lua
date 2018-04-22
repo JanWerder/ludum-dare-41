1930c5e43c1db25c27dc3ff3e1a2ac94f43a51f0
Jan <jackflash5500@gmail.com>
2018-04-22 16:53:17 +0200

    pause

--- objects/conversations.lua
+++ objects/conversations.lua
@@ -18,5 +18,25 @@ return {
             { name = "Gordon R.", color = {255,105,180}, message = {"You can certainly try. If you beat me, I'll consider it."}, avatar = gameAttack.gordon },
             { name = "You", color = {100,255,180}, message = {"Fair enough!"}, avatar = gameAttack.gordon },
         }
+    },
+    {--chef 2
+        {--paragraph 1
+        { name = "Jamie O.", color = {255,105,180}, message = {"Hey Panini Head!"}, avatar = gameAttack.gordon },
+        { name = "You", color = {100,255,180}, message = {"Hey, what's up!"}, avatar = gameAttack.gordon },
+        { name = "Jamie O.", color = {255,105,180}, message = {"What do you want, donkey head?!"}, avatar = gameAttack.gordon },
+        { name = "You", color = {100,255,180}, message = {"I want you to join my quest to assemble the best chef team in the world."}, avatar = gameAttack.gordon },
+        { name = "Jamie O.", color = {255,105,180}, message = {"What?! --You seriously suprise me, you surprise me how shit you are!"}, avatar = gameAttack.gordon }
+    },
+    {--paragraph 2
+        { name = "You", color = {100,255,180}, message = {"Ok, wait, maybe I can win you over with a joke!"}, avatar = gameAttack.gordon },
+        { name = "Jamie O.", color = {255,105,180}, message = {"I wish you would jump in the oven! That would make my life a lot easier."}, avatar = gameAttack.gordon },
+        { name = "You", color = {100,255,180}, message = {"No, listen: My friends bakery burned down... --Now his business is toast!"}, avatar = gameAttack.gordon },
+        { name = "Jamie O.", color = {255,105,180}, message = {"My Gran could do better--and she's dead."}, avatar = gameAttack.gordon },
+    },
+    {--paragraph 3
+        { name = "You", color = {100,255,180}, message = {"Ok, maybe my jokes aren't best, but if I defeat you, will you join me?"}, avatar = gameAttack.gordon },
+        { name = "Jamie O.", color = {255,105,180}, message = {"You can certainly try. If you beat me, I'll consider it."}, avatar = gameAttack.gordon },
+        { name = "You", color = {100,255,180}, message = {"Fair enough!"}, avatar = gameAttack.gordon },
     }
+}
 }
