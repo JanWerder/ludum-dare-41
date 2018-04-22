86281dede9174396cd8e63a491e207a92f4725ff
Jan <jackflash5500@gmail.com>
2018-04-22 17:24:50 +0200

    Finish conversations

--- objects/conversations.lua
+++ objects/conversations.lua
@@ -21,22 +21,42 @@ return {
     },
     {--chef 2
         {--paragraph 1
-        { name = "Jamie O.", color = {255,105,180}, message = {"Hey Panini Head!"}, avatar = gameAttack.gordon },
-        { name = "You", color = {100,255,180}, message = {"Hey, what's up!"}, avatar = gameAttack.gordon },
-        { name = "Jamie O.", color = {255,105,180}, message = {"What do you want, donkey head?!"}, avatar = gameAttack.gordon },
-        { name = "You", color = {100,255,180}, message = {"I want you to join my quest to assemble the best chef team in the world."}, avatar = gameAttack.gordon },
-        { name = "Jamie O.", color = {255,105,180}, message = {"What?! --You seriously suprise me, you surprise me how shit you are!"}, avatar = gameAttack.gordon }
+            { name = "Jamie O.", color = {255,105,180}, message = {"Oh hi there!"}, avatar = gameAttack.gordon },
+            { name = "You", color = {100,255,180}, message = {"Oh hi, Jamie!"}, avatar = gameAttack.gordon },
+            { name = "Jamie O.", color = {255,105,180}, message = {"What is going on here?!"}, avatar = gameAttack.gordon },
+            { name = "You", color = {100,255,180}, message = {"I want you to join my quest to assemble the best chef team in the world."}, avatar = gameAttack.gordon },
+            { name = "Jamie O.", color = {255,105,180}, message = {"Well, then let's get cracking! Show me how wicked you are!"}, avatar = gameAttack.gordon }
+        },
+        {--paragraph 2
+            { name = "You", color = {100,255,180}, message = {"Jamie, I have a joke for you, to convince you."}, avatar = gameAttack.gordon },
+            { name = "Jamie O.", color = {255,105,180}, message = {"Well, mate, pukka. Give me your best shot."}, avatar = gameAttack.gordon },
+            { name = "You", color = {100,255,180}, message = {"Why did the chef have to stop cooking?--He ran out of Thyme."}, avatar = gameAttack.gordon },
+            { name = "Jamie O.", color = {255,105,180}, message = {"Oh well--I think we have to see about me joining you."}, avatar = gameAttack.gordon },
+        },
+        {--paragraph 3
+            { name = "You", color = {100,255,180}, message = {"I haven't improved on my jokes, but will you join me anyways if I beat you?"}, avatar = gameAttack.gordon },
+            { name = "Jamie O.", color = {255,105,180}, message = {"Easy peasy, give it a try."}, avatar = gameAttack.gordon },
+            { name = "You", color = {100,255,180}, message = {"Alright!"}, avatar = gameAttack.gordon },
+        }
     },
-    {--paragraph 2
-        { name = "You", color = {100,255,180}, message = {"Ok, wait, maybe I can win you over with a joke!"}, avatar = gameAttack.gordon },
-        { name = "Jamie O.", color = {255,105,180}, message = {"I wish you would jump in the oven! That would make my life a lot easier."}, avatar = gameAttack.gordon },
-        { name = "You", color = {100,255,180}, message = {"No, listen: My friends bakery burned down... --Now his business is toast!"}, avatar = gameAttack.gordon },
-        { name = "Jamie O.", color = {255,105,180}, message = {"My Gran could do better--and she's dead."}, avatar = gameAttack.gordon },
-    },
-    {--paragraph 3
-        { name = "You", color = {100,255,180}, message = {"Ok, maybe my jokes aren't best, but if I defeat you, will you join me?"}, avatar = gameAttack.gordon },
-        { name = "Jamie O.", color = {255,105,180}, message = {"You can certainly try. If you beat me, I'll consider it."}, avatar = gameAttack.gordon },
-        { name = "You", color = {100,255,180}, message = {"Fair enough!"}, avatar = gameAttack.gordon },
+    {--chef 3
+        {--paragraph 1
+            { name = "Nigella  L.", color = {255,105,180}, message = {"Hello!"}, avatar = gameAttack.gordon },
+            { name = "You", color = {100,255,180}, message = {"Oh hello, Nigella!"}, avatar = gameAttack.gordon },
+            { name = "Nigella  L.", color = {255,105,180}, message = {"What is this?"}, avatar = gameAttack.gordon },
+            { name = "You", color = {100,255,180}, message = {"I want you to join my quest to assemble the best chef team in the world."}, avatar = gameAttack.gordon },
+            { name = "Nigella  L.", color = {255,105,180}, message = {"Oh dear!"}, avatar = gameAttack.gordon }
+        },
+        {--paragraph 2
+            { name = "You", color = {100,255,180}, message = {"Nigella, I have a special joke for you."}, avatar = gameAttack.gordon },
+            { name = "Nigella  L.", color = {255,105,180}, message = {"Ok, then. Continue."}, avatar = gameAttack.gordon },
+            { name = "You", color = {100,255,180}, message = {"Did you hear about the italian chef that died?--He pasta way."}, avatar = gameAttack.gordon },
+            { name = "Nigella  L.", color = {255,105,180}, message = {"Oh well, that joke was...--unique."}, avatar = gameAttack.gordon },
+        },
+        {--paragraph 3
+            { name = "You", color = {100,255,180}, message = {"Oh, that means you'll join me?"}, avatar = gameAttack.gordon },
+            { name = "Nigella  L.", color = {255,105,180}, message = {"I am always surprised when people read double entendres into my innocuous babble."}, avatar = gameAttack.gordon },
+            { name = "You", color = {100,255,180}, message = {"Good enough! I'll just have to defeat you then."}, avatar = gameAttack.gordon },
+        }
     }
-}
 }
