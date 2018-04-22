bb26c5ee8702f5c4de9bce453f14de34f1051b8d
philippthiele <philipp.thiele.94@gmail.com>
2018-04-22 21:24:57 +0200

    changed menu text

--- states/menu.lua
+++ states/menu.lua
@@ -4,24 +4,29 @@ end
 
 function menu:draw()
 	imageMenu = love.graphics.newImage("img/icon.png")
-	local menuPolygon = {80, 230, 420, 230, 420, 400, 80, 400}
+	local menuPolygon = {50, 175, 420, 175, 420, 435, 50, 435}
 	love.graphics.draw(imageMenu, 0, 0, 0, 1, 0.75)
 	love.graphics.push("all")
-	love.graphics.setColor(50,50,50,200)
+	love.graphics.setColor(50,50,50,225)
 	love.graphics.polygon("fill", menuPolygon)
 	love.graphics.pop()
 
 	
 	-- Text
-	suit.layout:reset(100,250)
-	suit.Label("Hello,", {align = "left"}, suit.layout:row(200,20))
-	suit.Label("it's my quest to assemble the best chef team", {align = "left"}, suit.layout:row(300,20))
-	suit.Label("in the world.", {align = "left"}, suit.layout:row(300,20))	
-	suit.Label("Want to help me? Press start!", {align = "left"}, suit.layout:row(200,20))
-	suit.layout:row(200,20)
-	
+	suit.layout:reset(60,186)
+	suit.Label("Hello there!", {align = "left"}, suit.layout:row(400,20))
+	suit.Label("Welcome to the world of chefs! My name is Spruce! ", {align = "left"}, suit.layout:row())
+	suit.Label("People call me the professor of chefs.", {align = "left"}, suit.layout:row())	
+	suit.Label("This world is inhabited by creatures called chefs.", {align = "left"}, suit.layout:row())
+	suit.Label("For some people, chefs are just people who make food.", {align = "left"}, suit.layout:row())
+	suit.Label("Others use them for fights.", {align = "left"}, suit.layout:row())
+	suit.Label("Myself...I study chefs as a profession.", {align = "left"}, suit.layout:row())
+	suit.layout:row(400,10)
+	suit.Label("Care to win the hearts of the best chefs", {align = "left"}, suit.layout:row(400,20))
+	suit.Label("and become the master of chefs?", {align = "left"}, suit.layout:row())
+	suit.layout:row(400,5)
 	-- Start Button
-	if suit.Button("Start", suit.layout:row(300,40)).hit then
+	if suit.Button("Start", suit.layout:row(350,40)).hit then
 		menu:start()
 	end
     suit.draw()
